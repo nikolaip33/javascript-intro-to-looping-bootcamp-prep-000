bb73eff436c1e1b442c11d041b99060913d83cb9
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-20 20:50:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,17 +2,19 @@ function maybeTrue() {
   return Math.random() >= 0.5
 }
 
-function forLoop(array){
-  for (let i = 0; i < 25; i++){
-    if (i===1){
-      console.log("I am 1 strange loop")
-      return array
+function forLoop(array) {
+  var size = array.length
+  for(let i = 0; i < 25; i++) {
+    if(i === 1) {
+      array[size+i]="I am 1 strange loop."
+      console.log(array[i])
     }
     else {
-      console.log("I am " + ${i} + " strange loops")
-      return array
+      array[size+i]=`I am ${i} strange loops.`
+      console.log(array[i])
     }
   }
+  return array
 }
 
 function whileLoop(n) {
